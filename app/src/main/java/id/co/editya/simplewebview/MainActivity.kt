c3709d5f5c4d05fa45a31d14bd81a1bb574c84b6
bintangeditya <bintangfarhandika@gmail.com>
2020-03-10 14:47:11 +0700

    override url loading

--- app/src/main/java/id/co/editya/simplewebview/MainActivity.kt
+++ app/src/main/java/id/co/editya/simplewebview/MainActivity.kt
@@ -1,6 +1,7 @@
 package id.co.editya.simplewebview
 
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,16 +12,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        wv_home.webViewClient = WebViewClient() // wv_home.setWbViewClinet(new WebViewClient())
-        wv_home.loadUrl("https://www.google.com")   // url request
+//        wv_home.webViewClient = WebViewClient() //  webclient default
+//        (java->) wv_home.setWebViewClient(new WebViewClient())
+
+        wv_home.webViewClient = CustomWeViewClient()  //  custom webviewclient
+        wv_home.loadUrl("https://www.google.com")   //  url request
     }
 
     override fun onBackPressed() {
-
         if (wv_home.canGoBack()) {
-            wv_home.goBack() // Goback di webview, kalau bukan berada pada page pertama webview
+            wv_home.goBack() // ketika back button di klik, lakukan goback di webview, jika tidak berada pada page pertama
         } else
             super.onBackPressed()
-
     }
+
+    inner class CustomWeViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {  // ketika hyperlink pada web di klik, tidak akan mengarah ke browser lain
+            view!!.loadUrl(url)
+            return true
+        }
+    }
+
+    /* (java->)
+    class CustomWeViewClient extend WebViewClient {
+          @Override
+          public boolean shouldOverrideUrlLoading(WebView view, String : url){
+              view.loadUrl(url)
+              return true
+          }
+     */
 }
